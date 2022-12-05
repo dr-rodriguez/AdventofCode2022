e1f37ec734706d152262735bef05e3a98cf645b0
dr-rodriguez <strakul@gmail.com>
2022-12-05 16:25:52 -0500

    day 5 star 1

---
+++ scripts/day5.jl
@@ -0,0 +1,63 @@
+# Day 5
+
+# sample
+input = """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"""
+
+stacks = [
+    ["Z","N"], 
+    ["M","C","D"],
+    ["P"]]
+
+# Read from file
+f = open("data/day5.txt", "r")
+input = read(f, String)
+close(f)
+
+stacks = [
+    split("GDVZJSB",""),
+    split("ZSMGVP",""),
+    split("CLBSWTQF",""),
+    split("HJGWMRVQ",""),
+    split("CLSNFMD",""),
+    split("RGCD",""),
+    split("HGTRJDSQ",""),
+    split("PFV",""),
+    split("DRSTJ","")
+    ]
+
+# Input preparation
+input = replace(input, "\r\n" => "\n")  # Windows new line
+input = split(input, "\n")
+
+for line in input
+    # Skip non-move lines
+    if !startswith(line, "move")
+        continue
+    end
+
+    # Identify move information
+    # println(line)
+    a = match(r"move (\d+) from (\d+) to (\d+)", line)
+    moves, ind_start, ind_end = parse.(Int32, a)
+    # println(moves, ind_start, ind_end)
+
+    # Perform moves
+    while moves > 0
+        crate = pop!(stacks[ind_start])
+        push!(stacks[ind_end], crate)
+        moves -= 1
+    end
+
+end
+println("Top of each stack:")
+for ln in stacks
+    print(last(ln))
+end
